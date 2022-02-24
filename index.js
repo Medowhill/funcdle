675a42ec2d3babea52edf3753367110675e940e8
Jaemin Hong <hjm0901@gmail.com>
2022-02-24 17:44:44 +0900

    update ui

--- index.js
+++ index.js
@@ -5,7 +5,85 @@ const delta = 0.5;
 const near = '#202020';
 const far = '#e0e0e0';
 
-var board, answer, points = [];
+const correctWord = validRand().toString();
+var board, points = [];
+let isGameOver = false;
+let guessNum = 0;
+let guessWord = '';
+
+const processKey = key => {
+	if (guessWord.length < 5 && key.match(/^[x0-9\+\-\*\/\^√㏑]$/)) {
+		guessWord += key;
+	} else if (guessWord.length > 0 && ['Backspace', 'Delete'].indexOf(key) !== -1) {
+		guessWord = guessWord.slice(0, -1);
+	} else if (guessWord.length == 5 && key == 'Enter') {
+    const f = parse(guessWord);
+		if (f !== null) {
+      guessGraph(parse(correctWord), f);
+
+			const guessStatuses = Array(guessWord.length).fill('wrong');
+			const almostTestLetters = correctWord.split('');
+
+			guessWord.split('').forEach((letter, index) => {
+				if (letter == correctWord[index]) {
+					guessStatuses[index] = 'correct';
+					almostTestLetters.splice(almostTestLetters.indexOf(letter), 1);
+				}
+			});
+
+			guessWord.split('').forEach((letter, index) => {
+				if (guessStatuses[index] == 'wrong' && almostTestLetters.indexOf(letter) !== -1) {
+					guessStatuses[index] = 'almost';
+					almostTestLetters.splice(almostTestLetters.indexOf(letter), 1);
+				}
+			});
+
+			guessStatuses.forEach((status, index) => {
+				const gbTileNode = document.querySelectorAll('.gb-row')[guessNum].childNodes[index];
+				gbTileNode.style.animationDelay = (100 * index) + 'ms';
+				gbTileNode.classList.add(status);
+				Array.from(document.querySelectorAll('.kb-row button')).find(n => n.textContent == guessWord[index]).classList.add(status);
+			});
+
+			if (guessStatuses.every(s => s == 'correct')) {
+				alert('You won in ' + (guessNum + 1) + ' guesses! Reload for a new word.');
+				isGameOver = true;
+			} else if (guessNum == 5) {
+				alert('You lost, the word was "' + correctWord + '". Reload for a new word.');
+				isGameOver = true;
+			} else {
+				guessNum++;
+				guessWord = '';
+			}
+		} else {
+			document.querySelectorAll('.gb-row')[guessNum].classList.add('invalid');
+			alert('Not a valid formula.');
+			document.querySelectorAll('.gb-row')[guessNum].classList.remove('invalid');
+		}
+	}
+
+	document.querySelectorAll('.gb-row')[guessNum].childNodes.forEach((node, index) => {
+		if (guessWord[index]) {
+			node.textContent = guessWord[index];
+			node.classList.add('filled');
+		} else {
+			node.textContent = '';
+			node.classList.remove('filled');
+		}
+	});
+};
+
+board = JXG.JSXGraph.initBoard('jxgbox', {
+  boundingbox: [-size, size, size, -size],
+  showCopyright: false,
+  showNavigation: false,
+  axis: true,
+  grid: true
+});
+
+document.querySelectorAll('.kb-row button').forEach(n => n.addEventListener('click', e => !isGameOver && processKey(e.target.textContent)));
+document.addEventListener('keydown', e => { if (e.keyCode === 191) e.preventDefault(); });
+document.addEventListener('keyup', e => !isGameOver && processKey(e.key));
 
 function validate(f) {
   const xs = (f.toString().match(/x/g) || []).length;
@@ -68,67 +146,3 @@ function guessGraph(f, guess) {
     }
   }
 }
-
-function guessColors(word, guess) { 
-  let colors = new Array(guess.length);
-
-  let unmatched = {};
-  for (let i = 0; i < word.length; i++) {
-      let letter = word[i];
-      if (letter === guess[i]) {
-          colors[i] = 'green';
-      } else {
-          unmatched[letter] = (unmatched[letter] || 0) + 1;
-      }
-  }
-
-  for (let i = 0; i < guess.length; i++) {
-      let letter = guess[i];
-      if (letter !== word[i]) {
-          if (unmatched[letter]) {
-              colors[i] = 'yellow';
-              unmatched[letter]--;
-          } else {
-              colors[i] = 'gray';
-          }
-      }
-  }
-
-  return colors;
-}
-
-function run() {
-  const s = $('#input').val();
-  if (s.length !== 5) {
-    alert('length != 5');
-    return;
-  }
-
-  const f = parse(s);
-  if (f === null) {
-    alert('cannot parse');
-    return;
-  }
-
-  guessGraph(answer, f);
-
-  const g = guessColors(answer.toString(), s);
-  const tds = _.zip(s.split(''), g).map(x => `<td class="${x[1]}">${x[0]}</td>`).join('');
-  const pf = `<td class="pretty">${pretty(f)}</td>`
-  $('#div-trial').append(`<table><tr>${tds}${pf}</tr></table>`);
-
-  $('#input').val('');
-}
-
-
-$(() => {
-  board = JXG.JSXGraph.initBoard('jxgbox', {
-    boundingbox: [-size, size, size, -size],
-    showCopyright: false,
-    showNavigation: false,
-    axis: true,
-    grid: true
-  });
-
-  answer = validRand();
-});
